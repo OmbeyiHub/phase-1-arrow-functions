256fe4a02e2a3c7cb41931f03bcbcbb61d178c2b
Brenda Ombeyi <brendaotieno2001@gmail.com>
2024-07-23 10:02:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = function (a){
+    return ( a / b)
+}
+let a = 2000
+let b = 100
+
+
+
+const square = (x) => (x * x)
+let x = 2
+
+
+const add = (z,y) => {
+console.log(`Adding ${z}`)
+console.log(`Adding ${y}`)
+return z + y
+}
+add (3,5)
